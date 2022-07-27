b47e2ee47cea85dc24e258c9adc42697a4fde4c2
Hendrik Wichern <git@hendrikwichern.de>
2022-07-27 18:07:18 +0200

    updated cube scene

--- components/cube/Renderer.vue
+++ components/cube/Renderer.vue
@@ -11,13 +11,56 @@ import Vue from 'vue';
 import { mapMutations, mapState } from 'vuex';
 import * as THREE from 'three';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { HalftonePass } from 'three/examples/jsm/postprocessing/HalftonePass.js';
 import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass.js';
-import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader.js';
-import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+const SIZE = 1; // dynamic scene size for edge debugging
+const OBJECTS = {
+    lights: [
+        {
+            position: { x: 50 * SIZE, y: 30 * SIZE, z: -15 * SIZE },
+            intensity: 40,
+            distance: 100 * SIZE,
+        },
+        {
+            position: { x: -32 * SIZE, y: 7 * SIZE, z: -8 * SIZE },
+            intensity: 7,
+            distance: 1000 * SIZE,
+        },
+    ],
+    cubes: [
+        {
+            position: { x: -0.7, y: 0, z: -0.75 },
+            rotation: { x: 0, y: 0.17, z: 0 },
+        },
+        {
+            position: { x: 1, y: 0, z: -5.5 },
+            rotation: { x: 0, y: -0.28, z: 0 },
+        },
+        {
+            position: { x: 3.4, y: 0, z: -11 },
+            rotation: { x: 0, y: 0.36, z: 0 },
+        },
+        {
+            position: { x: -4.5, y: 0, z: -14 },
+            rotation: { x: 0, y: 0.1, z: 0 },
+        },
+    ],
+};
+const HALFTONE_PARAMS = {
+    shape: 1,
+    radius: 12,
+    rotateR: Math.PI / 12,
+    rotateB: Math.PI / 12 * 2,
+    rotateG: Math.PI / 12 * 3,
+    scatter: 1,
+    blending: 1,
+    blendingMode: 1,
+    greyscale: false,
+    disable: false
+};
 
 export default Vue.extend({
     computed: {
@@ -45,156 +88,120 @@ export default Vue.extend({
                 y: height,
             }
         },
-        initThree() {
-            let params = {
-                exposure: this.settings.exposure,
-                bloomThreshold: 0,
-                bloomStrength: this.settings.bloomStrength,
-                bloomRadius: this.settings.bloomRadius,
-            };
-
-            const CAMERA_DISTANCE = 50;
+        async initThree() {
+            const cameraDistance = 42 * SIZE;
             const scene = new THREE.Scene();
-            const camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 2000);
-
-            camera.useQuaternion = true;
-            var am_light = new THREE.AmbientLight(0x555555); // soft white light
-            scene.add(am_light);
+            const camera = new THREE.PerspectiveCamera(12, window.innerWidth / window.innerHeight, 0.1, 2000 * SIZE);
 
             scene.background = new THREE.Color(0x000000);
-            const renderer = new THREE.WebGLRenderer({ antialias: true });
-            renderer.setPixelRatio(window.devicePixelRatio);
-            // renderer.toneMapping = THREE.ReinhardToneMapping;
-            renderer.setSize(window.innerWidth, window.innerHeight);
+            const renderer = new THREE.WebGLRenderer({ antialias: true, logarithmicDepthBuffer: true });
             renderer.shadowMap.enabled = true;
             renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+            renderer.setPixelRatio(window.devicePixelRatio);
+            renderer.setSize(window.innerWidth, window.innerHeight);
             this.$refs.main.appendChild(renderer.domElement);
-/*
-            const light = new THREE.SpotLight();
-            light.intensity = 10;
-            light.target = scene;
-            light.castShadow = true;
-            light.position.set(100, 15, -10);
-            // light.position.set(0,0,2);
-            // light.rotation.set(0.25,40,20);
-            // light.shadow.mapSize.width = 512;
-            // light.shadow.mapSize.height = 512;
-            // light.shadow.camera.near = 0.25;
-            // light.shadow.camera.far = 100;
-            light.shadow.camera.fov = 1;
-            // light.shadow.camera.position.set(200,200,0);
-            // light.shadow.camera.rotation.set(-0.1,1,0);
-            scene.add(light);
-*/
 
-            const bulbGeometry = new THREE.SphereBufferGeometry(0.02, 16, 8);
-            const bulbLight = new THREE.DirectionalLight(0xffffff, 5, 100, 2);
-            const bulbMat = new THREE.MeshStandardMaterial({
+            scene.background = new THREE.Color(0x000000);
+
+            const bulbGeometry = new THREE.SphereBufferGeometry(0.02 * SIZE, 16, 8);
+            const bulbMaterial = new THREE.MeshStandardMaterial({
                 emissive: 0xffffee,
                 emissiveIntensity: 1,
                 color: 0x000000
             });
-            bulbLight.add(new THREE.Mesh(bulbGeometry, bulbMat));
-            bulbLight.position.set(10, 5, -2);
-            bulbLight.castShadow = true;
-            scene.add(bulbLight); 
-            
-            const bulbLight2 = new THREE.DirectionalLight(0xffffff, 5, 100, 2);
-            bulbLight2.add(new THREE.Mesh(bulbGeometry, bulbMat));
-            bulbLight2.position.set(-15, 5, -5);
-            bulbLight2.castShadow = true;
-            scene.add(bulbLight2);
-            // const helper = new THREE.CameraHelper(light.shadow.camera);
-            // scene.add(helper);
+            OBJECTS.lights.forEach(light => {
+                scene.add(this.getLight(bulbGeometry, bulbMaterial, light.position, light.intensity, light.distance));
+            });
 
-            // new OrbitControls(camera, renderer.domElement)
+            const cubeGeometry = new THREE.BoxBufferGeometry(4 * SIZE, 4 * SIZE, 4 * SIZE);
+            const cubeMaterial = new THREE.MeshPhongMaterial({ color: 0x222222 });
+            OBJECTS.cubes.forEach((cubeSettings, i) => {
+                const cube = this.getCube(cubeGeometry, cubeMaterial, cubeSettings.position, cubeSettings.rotation);
+                scene.add(cube);
+            });
+
+            scene.add(this.getPlane());
+
+            const halftonePass = new HalftonePass(window.innerWidth, window.innerHeight, HALFTONE_PARAMS);
             
-            const composer = new EffectComposer(renderer);
             const renderPass = new RenderPass(scene, camera);
-            composer.addPass(renderPass);
+            const composer = new EffectComposer(renderer);
 
             const bokehPass = new BokehPass(scene, camera, {
-                focus: 2,
-                aperture: 0.00006,
-                maxblur: 0.03,
+                focus: cameraDistance,
+                aperture: 0.0005,
+                maxblur: 1,
                 width: window.innerWidth,
                 height: window.innerHeight
             });
-            bokehPass.renderToScreen = true;
-            composer.addPass(bokehPass);
+            bokehPass.needsSwap = true; // important to enable combination with other passes
 
-            // const RGBShift = new ShaderPass(RGBShiftShader);
-            // RGBShift.uniforms['amount'].value = 0;
-            // composer.addPass(RGBShift);
+            composer.addPass(renderPass);
+            // composer.addPass(bokehPass);
+            composer.addPass(halftonePass);
 
-
-            const geometry = new THREE.BoxBufferGeometry(4, 4, 4);
-            const material = new THREE.MeshPhongMaterial({ color: 0x222222 });
-            const cube = new THREE.Mesh(geometry, material);
-            const cube2 = new THREE.Mesh(geometry, material);
-            const cube3 = new THREE.Mesh(geometry, material);
-            cube.position.set(5, 0, -15);
-            cube2.position.set(10, 0, -50);
-            cube3.position.set(2, 0, -3);
-            cube.rotation.set(0, -5, 0);
-            cube3.rotation.set(0, 0.1, 0);
-            cube.castShadow = true;
-            cube.receiveShadow = true;
-            cube2.castShadow = true;
-            cube2.receiveShadow = true;
-            cube3.castShadow = true;
-            cube3.receiveShadow = true;
-            scene.add(cube);
-            scene.add(cube2);
-            scene.add(cube3);
-            bulbLight.target = cube3;
-            bulbLight2.target = cube3;
-
-            var geo = new THREE.PlaneBufferGeometry(2000, 2000, 8, 8);
-            var mat = new THREE.MeshPhongMaterial({ color: 0x333333, side: THREE.DoubleSide });
-            var plane = new THREE.Mesh(geo, mat);
-            plane.receiveShadow = true;
-            plane.position.set(0,-2.03,0);
-            plane.rotation.x = -(Math.PI / 2);
-            plane.rotation.z = -(Math.PI / 2);
-            scene.add(plane);
-
-            camera.position.set(0, 7, CAMERA_DISTANCE); // lift camera...
-            camera.rotation.set(-0.12, 0, 0); // ... and look down
+            camera.position.set(0.1 * SIZE, 7.8 * SIZE, cameraDistance); // lift camera...
+            camera.rotation.set(-0.16, 0, 0); // ...and look down
 
             const animate = () => {
                 requestAnimationFrame(animate);
-                if (this.stopMultiplicator !== 0) {
-                    updateContent();
-                }
+
                 composer.render();
             }
 
-            let rotation = 0;
-
-            const updateContent = () => {
-                // rotation = rotation + (6 * this.settings.speed / 180);
-                // cube.rotation.set(0, rotation, 0);
-                // camera.position.set(0, 0, CAMERA_DISTANCE);
-                // if (this.settings.showSecondary > 0.5) {
-                //     RGBShift.uniforms['amount'].value = Math.random() / 50;
-                // } else {
-                //     RGBShift.uniforms['amount'].value = 0;
-                // }
-            }
+            new OrbitControls(camera, renderer.domElement);
             animate();
         },
-    },
-    mounted() {
-        this.initThree();
-
-        document.addEventListener('keyup', event => {
-            if (event.keyCode === 32 && this.stopMultiplicator !== 0) {
-                this.SET_STOP_MULTIPLICATOR(0);
-            } else {
-                this.SET_STOP_MULTIPLICATOR(1);
+        getLight(geometry, material, position, intensity, distance) {
+            const bulbLight = new THREE.PointLight(0xffffff, intensity, distance, 2);
+            bulbLight.add(new THREE.Mesh(geometry, material));
+            bulbLight.position.set(position.x, position.y, position.z);
+            bulbLight.castShadow = true;
+            bulbLight.shadow.radius = 4;
+            bulbLight.shadow.mapSize.width = 2048 * 2;
+            bulbLight.shadow.mapSize.height = 2048 * 2;
+            bulbLight.shadow.blurSamples = 8;
+            
+            return bulbLight;
+        },
+        getCube(geometry, material, position, rotation) {
+            const cube = new THREE.Mesh(geometry, material);
+            cube.position.set(position.x * SIZE, position.y * SIZE, position.z * SIZE);
+            if (rotation) {
+                cube.rotation.set(rotation.x, rotation.y, rotation.z);
             }
-        });
+            cube.castShadow = true;
+            cube.receiveShadow = true;
+
+            return cube;
+        },
+        getPlane() {
+            const planeGeometry = new THREE.PlaneBufferGeometry(2000 * SIZE, 2000 * SIZE, 8, 8);
+            const planeMaterial = new THREE.MeshPhongMaterial({
+                color: 0x333333
+            });
+            const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+            plane.receiveShadow = true;
+            plane.position.set(0, -2.001 * SIZE, 0);
+            plane.rotation.x = -(Math.PI / 2);
+            plane.rotation.z = -(Math.PI / 2);
+
+            return plane;
+        }
+    },
+    async mounted() {
+        if (process.client) {
+            await this.initThree();
+
+            document.addEventListener('keyup', event => {
+                if (event.keyCode === 32 && this.stopMultiplicator !== 0) {
+                    this.SET_STOP_MULTIPLICATOR(0);
+                } else {
+                    this.SET_STOP_MULTIPLICATOR(1);
+                }
+            });
+        }
+
     },
 });
 </script>
