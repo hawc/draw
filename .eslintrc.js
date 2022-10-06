d216e88715be5c88801e13c887a756fd38076fd3
Hendrik Wichern <git@hendrikwichern.de>
2022-10-06 22:32:45 +0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,25 @@ module.exports = {
     rules: {
         "indent": 'off',
         "@typescript-eslint/indent": ["error", 4],
+        "semi": "off",
+        "@typescript-eslint/semi": ["error", "always"],
+        "comma-dangle": ["error", "always-multiline"],
+        "func-call-spacing": "off",
+        "space-before-function-paren": ["error", "never"],
+        "@typescript-eslint/func-call-spacing": ["error", "never"],
+        "vue/html-indent": ["error", 4],
+        "vue/html-self-closing": ["error", {
+            "html": {
+                "void": "always",
+                "normal": "never",
+                "component": "always"
+            },
+            "svg": "always",
+            "math": "always"
+        }],
+        "vue/html-closing-bracket-newline": ["error", {
+            "singleline": "never",
+            "multiline": "never"
+        }]
     }
 }
