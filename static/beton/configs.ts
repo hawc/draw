cc42bcbed9fa9eab4db69071fac78adbb49bb24c
Hendrik Wichern <git@hendrikwichern.de>
2022-10-14 22:09:58 +0200

    added configs

---
+++ static/beton/configs.ts
@@ -0,0 +1,115 @@
+
+const configs = [
+    {
+        name: 'entrance low 240',
+        elements: {
+            basement: [['door240-01.obj'], ['door240-02.obj'], ['door240-03.obj'], ['door240-04.obj']],
+            ground: [['spacer-half240-01.obj']],
+            rooms: [['room240-01.obj'], ['room240-02.obj']],
+            roof: [['roof240-01.obj'], ['roof240-02.obj'], ['roof240-03.obj']],
+        },
+    },
+    {
+        name: 'entrance high 240',
+        elements: {
+            basement: [['basement240-01.obj'], ['basement240-02.obj']],
+            ground: [['door240-01.obj'], ['door240-03.obj'], ['door240-04.obj']],
+            rooms: [['room240-01.obj'], ['room240-02.obj']],
+            roof: [['roof240-01.obj'], ['roof240-02.obj'], ['roof240-03.obj']],
+        },
+    },
+    {
+        name: 'living 240',
+        elements: {
+            basement: [['basement240-01.obj'], ['basement240-02.obj']],
+            ground: [],
+            rooms: [['room240-01.obj'], ['room240-02.obj']],
+            roof: [['roof240-01.obj'], ['roof240-02.obj'], ['roof240-03.obj']],
+        },
+    },
+    {
+        name: 'living 360',
+        elements: {
+            basement: [['basement360-01.obj'], ['basement360-02.obj']],
+            ground: [],
+            rooms: [['room360-01.obj'], ['room360-02.obj']],
+            roof: [['roof360-01.obj'], ['roof360-02.obj'], ['roof360-03.obj']],
+        },
+    },
+    {
+        name: 'living 600',
+        elements: {
+            basement: [['basement600-01.obj'], ['basement600-02.obj']],
+            ground: [],
+            rooms: [['room600-01.obj'], ['room600-02.obj'], ['room600-03.obj'], ['room240-01.obj', 'room360-01.obj']],
+            roof: [['roof300-01.obj', 'roof300-01.obj'], ['roof300-01.obj', 'roof300-02.obj'], ['roof300-02.obj', 'roof300-01.obj']],
+        },
+    },
+];
+
+/*
+const elementConfig = {
+    240: [
+        {
+            type: 'entrance',
+            configs: [
+                {
+                    name: 'low',
+                    elements: {
+                        basement: [['door240-01.obj'], ['door240-02.obj']],
+                        ground: ['half240-01.obj'],
+                        rooms: [['room240-01.obj'], ['room240-02.obj']],
+                        roof: [['roof240-01.obj'],['roof240-02.obj'],['roof240-03.obj']],
+                    },
+                },
+                {
+                    name: 'high',
+                    elements: {
+                        basement: [['basement240-01.obj'],['basement240-02.obj']],
+                        ground: ['door240-01.obj'],
+                        rooms: [['room240-01.obj'], ['room240-02.obj']],
+                        roof: [['roof240-01.obj'],['roof240-02.obj'],['roof240-03.obj']],
+                    },
+                },
+            ],
+        },
+        {
+            type: 'living',
+            configs: [
+                {
+                    name: 'living',
+                    elements: {
+                        basement: [['basement240-01.obj'],['basement240-02.obj']],
+                        ground: [],
+                        rooms: [['room240-01.obj'], ['room240-02.obj']],
+                        roof: [['roof240-01.obj'],['roof240-02.obj'],['roof240-03.obj']],
+                    },
+                },
+            ],
+        },
+    ],
+    360: [
+        {
+            type: 'living',
+            configs: [
+                {
+                    name: 'living',
+                    elements: {
+                        basement: [['basement360-01.obj'],['basement360-02.obj']],
+                        ground: [],
+                        rooms: [['room360-01.obj'], ['room360-02.obj']],
+                        roof: [['roof360-01.obj'],['roof360-02.obj'],['roof360-03.obj']],
+                    },
+                },
+            ],
+        },
+    ],
+    600: [
+
+    ],
+}
+*/
+
+export {
+    configs,
+};
