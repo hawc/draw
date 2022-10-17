4e2ff5f17ddb84d070ea77239c1d3e7975d0e853
Hendrik Wichern <git@hendrikwichern.de>
2022-10-17 21:02:33 +0200

    added debug view

---
+++ pages/beton/debug.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <Renderer />
+        <Sender 
+            standalone 
+            :controllers="defaults" 
+            class="sender"></Sender>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapMutations } from 'vuex';
+import Receiver from '@/components/Receiver.vue';
+import Renderer from '@/components/beton/Renderer.vue';
+import Sender from '@/components/Sender.vue';
+import { defaults } from '~/assets/beton/defaults';
+
+export default Vue.extend({
+    name: 'IndexPage',
+    components: {
+        Receiver,
+        Renderer,
+        Sender,
+    },
+    data() {
+        return {
+            defaults,
+        };
+    },
+    methods: {
+        ...mapMutations([
+            'POPULATE_STORE',
+        ]),
+    },
+    beforeMount() {
+        this.POPULATE_STORE(defaults);
+    },
+});
+</script>
+
+<style>
+    .sender {
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+</style>
